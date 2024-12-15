058fba6a31a36238ddd6de25e44f8425f14e12ff
frank <frankcampos85@gmail.com>
2024-12-14 21:27:46 -0600

    Refactor footer positioning, enhance search query handling, and implement API for fetching posts

--- app/component/Footer.tsx
+++ app/component/Footer.tsx
@@ -3,7 +3,7 @@ import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className="relative bottom-0 w-full h-25 text-tennessee-orange py-8 flex justify-center space-x-4">
+    <div className="fixed bottom-0 w-full h-25 text-tennessee-orange py-8 flex justify-center space-x-4">
       <Link href="https://github.com/frankcampos" target="_blank" rel="noopener noreferrer">
         <FaGithub className="text-2xl hover:text-white" />
       </Link>
