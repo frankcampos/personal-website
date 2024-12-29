82de55282c4e9336fa4b7725efdb70c421d4b172
frank <frankcampos85@gmail.com>
2024-12-28 21:16:34 -0600

    Fix resume download error handling and disable button during loading

--- app/component/AboutMeCard.tsx
+++ app/component/AboutMeCard.tsx
@@ -11,15 +11,15 @@ const AboutMeCard = () => {
       const response = await fetch('/api/downloadResume');
       if (response.status == 200){
         const data = await response.json();
-        console.log(data.url);
         window.open(data.url, '_blank')
-        setLoading(false);
       }else{
         console.error('Failed to fetch resume URL')
+        setLoading(false)
       }
     }catch(error){  
+      console.error('Error downloading resume:', error);
+    } finally{
       setLoading(false);
-      console.log("error:",error)
     }
   }
   return (
@@ -41,7 +41,7 @@ const AboutMeCard = () => {
           <p className="mt-4 text-gray-400">
             Latino, living in Nashville since 2006, and currently seeking a junior full-stack web developer position or an internship. I love learning new technologies and am currently focusing on TypeScript, MongoDB, React Query, and more. I’ve worked in various trades, mainly as a painter, and have been using Linux since 2015 after rescuing an old laptop when I could no longer use Windows.
           </p>
-          <button onClick={handleDownloadResume} className="mt-6 bg-tennessee-orange hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+          <button  disabled = {loading}onClick={handleDownloadResume} className="mt-6 bg-tennessee-orange hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
             <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
               <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
             </svg>
